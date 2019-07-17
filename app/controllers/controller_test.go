07e5e638c11decf7305d7b7512180251e219bb8f
cajecasu <cjcs1977@gmail.com>
2019-07-17 14:23:23 -0500

    por fin

--- app/controllers/controller_test.go
+++ app/controllers/controller_test.go
@@ -55,7 +55,7 @@ func TestGetNumeral(t *testing.T) {
 		t.Error("Expected response code to be 200")
 	}
 
-	expected := `{"De Romano A Numero-mejorado-Ingress-4":40}`
+	expected := `{"De Romano A Numero-mejorado-Ingress-5":40}`
 	body := strings.TrimSpace(rr1.Body.String())
 
 	if body != expected {
